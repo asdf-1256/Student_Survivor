78dddf8f3e26fd2478416bf35ffe7021753e9116
rlatldnd1 <sw001128@naver.com>
2023-10-05 00:36:47 +0900

    스킬_운체 구현
    Bullet의 달라지는 Collider는 자식 오브젝트 2개로 따로 구현
    - 맵 밖으로 벗어나는 경우에 대해 따로 구현해줘야 함.
    - OS 로봇은 적이 있었던 방향으로 무한 직진
    
    주의사항
    자식 오브젝트 : 콜라이더 + 리지드바디 -> 부모와 따로 취급
    
    자식 오브젝트 : 콜라이더만 있다 -> 부모의 콜라이더로 취급
    
    but Enemy에서 트리거되는 컬리젼 대상은 자식 오브젝트의 콜라이더로 인식

--- Assets/Undead Survivor/Codes/Skills/Bullet_OS.cs
+++ Assets/Undead Survivor/Codes/Skills/Bullet_OS.cs
@@ -11,13 +11,13 @@ public class Bullet_OS : MonoBehaviour
     public float duration;
     public float damage;
 
-    //Collider2D coll;
+    Collider2D[] colls; // 자식 오브젝트에 있는 콜라이더들
     SpriteRenderer spriteRenderer;
     Rigidbody2D rigid;
 
     private void Awake()
     {
-        //coll = GetComponent<Collider2D>();
+        colls = GetComponentsInChildren<Collider2D>(); // 0:OS모드, 1:폭발모드
         spriteRenderer = GetComponent<SpriteRenderer>();
         rigid = GetComponent<Rigidbody2D>();
     }
@@ -25,7 +25,8 @@ public class Bullet_OS : MonoBehaviour
     public void Init(float damage, Vector3 dir)
     {
         this.damage = damage;
-        rigid.velocity = dir * 5f; // 속도 일단 5로 둬. 하드코딩
+        
+        rigid.velocity = dir * 1f; // 속도 일단 1로 둬. 하드코딩
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,33 +35,40 @@ public class Bullet_OS : MonoBehaviour
             return;
         rigid.velocity = Vector2.zero;
 
-        spriteRenderer.sprite = sprites[1];//이미지를 일단 커피로 변경 -> 추후 폭발로 바꿔
+        spriteRenderer.sprite = sprites[1];//이미지를 일단 커피로 변경 -> 추후 폭발로 바꿔야 함
 
-        StartCoroutine(TimerRoutine(() => { gameObject.SetActive(false); }));
+
+        StartCoroutine(ExplosionRoutine(() => { gameObject.SetActive(false); }));
         
     }
-    private void OnTriggerExit2D(Collider2D collision)
+    /*private void OnTriggerExit2D(Collider2D collision) // 이거 없으니까 되네
     {
         if (!collision.CompareTag("Area"))
             return;
-
+        Debug.Log("OS 로봇 밖으로 나감");
         gameObject.SetActive(false);
-    }
+    }*/
     private void OnDisable()
     {
+
         spriteRenderer.sprite = sprites[0];
         transform.position = new Vector3(0, 0, 0);
         // transform.rotation = Quaternion.identity;
-        // StopAllCoroutines();
     }
-    IEnumerator TimerRoutine(System.Action done)
+    IEnumerator ExplosionRoutine(System.Action done)
     {
+        colls[1].enabled = true;
+        colls[0].enabled = false;
+
         float timer = 0f;
         while (timer <= duration)
         {
             timer += Time.deltaTime;
             yield return null;
         }
+
+        colls[0].enabled = true;
+        colls[1].enabled = false;
 
         done.Invoke();
     }
