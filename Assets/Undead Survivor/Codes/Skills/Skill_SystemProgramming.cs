d944e8d893178dc305f17453a6dbeecddc28e8e9
rlatldnd1 <sw001128@naver.com>
2023-10-08 22:14:16 +0900

    시보 기능 구현으로 다시 되돌아감

--- Assets/Undead Survivor/Codes/Skills/Skill_SystemProgramming.cs
+++ Assets/Undead Survivor/Codes/Skills/Skill_SystemProgramming.cs
@@ -6,7 +6,6 @@ public class Skill_SystemProgramming : MonoBehaviour
 {
     public float cooltime = 5f;
     public float timer = 5f;
-    public float speed = 7f;
 
     void Update()
     {
@@ -19,20 +18,7 @@ public class Skill_SystemProgramming : MonoBehaviour
     }
     void Fire()
     {
-        Transform target = GameManager.Instance.player.scanner.nearestTarget;
-        if (target == null)
-        {
-            return;
-        }
-        Transform player = GameManager.Instance.player.transform;
-
-        Transform bullet = GameManager.Instance.pool.Get(15).transform;
-        bullet.position = player.position;
-
-        Vector3 dir = target.position - player.position;
-        transform.LookAt(dir);
-
-        bullet.GetComponent<Rigidbody2D>().velocity = dir.normalized * speed;
+        GameManager.Instance.pool.Get(15);
     }
 
 }
