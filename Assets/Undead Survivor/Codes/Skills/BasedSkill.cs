847c28a01634e7be9a3471fde0926f335cd508dc
rlatldnd1 <sw001128@naver.com>
2023-10-12 16:04:21 +0900

    ^^

--- Assets/Undead Survivor/Codes/Skills/BasedSkill.cs
+++ Assets/Undead Survivor/Codes/Skills/BasedSkill.cs
@@ -51,7 +51,7 @@ public class BasedSkill : MonoBehaviour
         this.skillData = skillData;
         level = 0;
         Debug.Log("여기는 BasedSkill의 Init함수 내부입니다");
-        name = "SKILL " + skillData.skillID; // 오브젝트 name을 설정하는거임
+        name = "SKILL " + skillData.skillName; // 오브젝트 name을 설정하는거임
         transform.parent = player.transform;
         transform.localPosition = Vector3.zero;
 
