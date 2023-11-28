8873154721a6274940db118fb9c7de2fd5456d43
hasunghun <hshs8094@naver.com>
2023-11-28 16:00:56 +0900

    부활 돈 사라지게

---
+++ Assets/Undead Survivor/Codes/Retry_Button.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Retry_Button : MonoBehaviour
+{
+
+    public DataManager data;
+
+    private void Start()
+    {
+        UpdateButtonInteractable();
+    }
+
+    private void UpdateButtonInteractable()
+    {
+        if (DataManager.Instance.money <= 0)
+        {
+            GetComponent<Button>().interactable = false;
+            Debug.Log("돈이 부족합니다.");
+        }
+        else
+        {
+            GetComponent<Button>().interactable = true;
+            data.money = data.money - 1;
+        }
+    }
+}
